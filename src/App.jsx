df39765dac1f973064309634ba84ec18033bfdb5
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-04-09 14:12:32 +0200

    feat: complete milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
+import { useState } from 'react';
 import articles from './data/articles.json';
 
 function App() {
+
+  const [article, setArticle] = useState(articles);
+  const [newArticle, setNewArticle] = useState('');
+
+  const updateArticles = e => {
+    e.preventDefault();
+
+    const newObjArticle = {
+      id: article.at(-1).id + 1,
+      title: newArticle
+    };
+
+    setArticle([...article, newObjArticle]);
+    setNewArticle('');
+  };
+
   return (
     <>
-      {articles.map(article => <li key={article.id}>{article.title}</li>)}
-      <form>
-        <input type="text" />
+      <ul>
+        {article.map(article => <li key={article.id}>{article.title}</li>)}
+      </ul>
+
+      <form onSubmit={updateArticles}>
+        <input type="text" value={newArticle} onChange={e => setNewArticle(e.target.value)} />
+        <button>Aggiungi Articolo</button>
       </form>
     </>
   );
